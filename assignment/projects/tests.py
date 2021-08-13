49f533ad2a999d7eeecbee2cfcb4254c5c0b5df5
s.chakraborty2 <s.chakraborty2@globallogic.com>
2021-08-13 12:50:23 +0530

    Resource API testcases added

--- assignment/projects/tests.py
+++ assignment/projects/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from rest_framework import response
+import rest_framework
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.response import Response
 from rest_framework import status
@@ -45,6 +46,52 @@ class ProjectAPITestCase(APITestCase):
         response = self.client.delete(f'/api/project/{pro.id}/')
         count2 = Project.objects.all().count()
         self.assertEqual(count1 - 1, count2)
+
+
+class ResourceAPITestCase(APITestCase):
+    def setUp(self):
+        self.default_project = Project.objects.create(name='Resource Pool', start_date='2021-09-20',
+        end_date = '2023-09-20')
+        self.default_project.save()
+        self.project = Project.objects.create(name='Zipari', start_date='2021-09-20',
+        end_date = '2023-09-20')
+        self.project.save()
+
+        self.user = Resource.objects.create(email = 'dummy@gmail.com', name='dummy user', experience = 1,
+        password = 'abc123', project = self.default_project)
+        self.user.save()
+        self.client = APIClient()
+
+    def test_createUser(self):
+        count1 = Resource.objects.all().count()
+        user_dict = dict(name='dummy user 2', email = 'dummyUser@gmail.com',
+        password = 'abc123', experience = 2, project = self.default_project.id)
+        response = self.client.post('/api/user/',user_dict)
+        count2 = Resource.objects.all().count()
+        self.assertEqual(count1+1, count2)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_getUser(self):
+        user_id = self.user.id 
+        response = self.client.get(f'/api/user/{user_id}/')
+        self.assertEqual(self.user.name, response.data['name'])
+
+    def test_updateUser(self):
+        user_id = self.user.id 
+        user_dict = dict(name='updated name')
+        response = self.client.put(f'/api/user/{user_id}/', user_dict)
+        response = self.client.get(f'/api/user/{user_id}/')
+        self.assertEqual(response.data['name'], 'updated name')
+
+    def test_deleteUser(self):
+        user_id = self.user.id
+        count1 = Resource.objects.all().count()
+        response = self.client.delete(f'/api/user/{user_id}/')
+        count2 = Resource.objects.all().count()
+        self.assertEqual(count1-1, count2)
+        
+
+        
 
 
         
